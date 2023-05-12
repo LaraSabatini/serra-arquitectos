506bb45002095f1902cb03c0e144a3fc671ccd20
Lara <sabatinilara@gmail.com>
2023-05-12 13:05:52 -0300

    added theme and fonts

---
+++ src/theme/index.ts
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    black: "#273031",
+    white: "#F5F5F5",
+    primary: "#E4E7B5",
+  },
+  fontSizes: {
+    xl: "36px",
+    l: "20px",
+    m: "16px",
+    s: "14px",
+    xs: "12px",
+  },
+}
