4b7d777ce904173412abaed01ab6383239712eb3
Lara <sabatinilara@gmail.com>
2023-05-16 12:27:30 -0300

    removed logo options

--- src/views/Loaded/index.tsx
+++ src/views/Loaded/index.tsx
@@ -1,18 +1,11 @@
 import React from "react"
-import { useRouter } from "next/router"
 import Logo from "@components/Logo"
-import Logo2 from "@components/Logo2"
-import Logo3 from "@components/Logo3"
 import Container from "./styles"
 
 function LoadedView() {
-  const router = useRouter()
-  // a
   return (
     <Container>
-      {router.query.logo === undefined && <Logo dimesion="large" />}
-      {router.query.logo === "2" && <Logo2 dimesion="large" />}
-      {router.query.logo === "3" && <Logo3 dimesion="large" />}
+      <Logo dimesion="large" />
     </Container>
   )
 }
