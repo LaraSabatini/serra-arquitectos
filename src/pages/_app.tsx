feab59f4252e27128b492cebe34d7fff1a98626e
Lara <sabatinilara@gmail.com>
2023-05-17 10:53:41 -0300

    configured new dependencies, added font and created provider for antd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,25 @@ import type { AppProps } from "next/app"
 import Head from "next/head"
 import GlobalStyle from "@theme/styles"
 import "../theme/fonts.css"
+import { ConfigProvider } from "antd"
+import theme from "@theme/index"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: `${theme.colors.black}`,
+          fontFamily: "Regular",
+        },
+      }}
+    >
       <Head>
         <title>Serra Arquitectos</title>
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ConfigProvider>
   )
 }
 
