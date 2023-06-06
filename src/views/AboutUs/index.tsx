31b3093f8a28db23b4f8d8931481bfe30c255de7
Lara <sabatinilara@gmail.com>
2023-06-06 16:53:05 -0300

    distinciones

--- src/views/AboutUs/index.tsx
+++ src/views/AboutUs/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router"
 import Partners from "./Partners"
 import Studio from "./Studio"
 import Processes from "./Processes"
+import Distinctions from "./Distinctions"
 
 function AboutUsView() {
   const router = useRouter()
@@ -14,6 +15,7 @@ function AboutUsView() {
       {categoryId === 200 && <Partners />}
       {categoryId === 201 && <Studio />}
       {categoryId === 203 && <Processes />}
+      {categoryId === 204 && <Distinctions />}
     </>
   )
 }
